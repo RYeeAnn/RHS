1073f99fbc65a96de269cbf4a6073a27f6c156af
Ryan <ryeean16@gmail.com>
2025-10-02 16:52:02 -0700

    scroll to top update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import Services from './pages/Services';
 import Appointment from './pages/Appointment';
@@ -10,6 +11,7 @@ import './App.css';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="App">
         <Navbar />
         <main>
